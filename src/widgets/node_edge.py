4d58f78d234511abdafb4ad9f8a950192b9fdd16
virgilsisoe <virgilsisoe@gmail.com>
2022-04-02 12:53:17 -0400

    drag the edge to mouse position

--- src/widgets/node_edge.py
+++ src/widgets/node_edge.py
@@ -1,21 +1,31 @@
-from PySide2.QtCore import QPointF, Qt, QRectF
+from PySide2.QtCore import QPointF
 from PySide2.QtGui import QPen, QPainterPath, QColor
 
 from PySide2.QtWidgets import (
     QGraphicsPathItem,
-    QGraphicsItem
+    QGraphicsItem,
+    QGraphicsView
 )
+
+from src.utils import class_id
 
 
 class NodeEdgeGraphics(QGraphicsPathItem):
-    def __init__(self, start_socket, end_socket):
-        super().__init__(start_socket.socket_graphics)
 
+    def __init__(self, view, start_socket, end_socket):
+        super().__init__(start_socket)
+
+        self.view: QGraphicsView = view
         self.start_socket = start_socket
         self.end_socket = end_socket
 
+        self.mouse_pos = (start_socket.get_position().x(),
+                          start_socket.get_position().y())
+
         self._set_colors()
         self._set_flags()
+
+        self.view.mouse_position.connect(self._set_mouse)
 
     def _set_colors(self):
         self._pen_selected = QPen(QColor('#DD8600'))
@@ -29,9 +39,19 @@ class NodeEdgeGraphics(QGraphicsPathItem):
         # FIXME: edge stays on top of the socket
         self.setZValue(-1.0)
 
+    def _set_mouse(self, x, y):
+        # FIXME: ugly
+        self.mouse_pos = (x, y)
+
     def paint(self, painter, option, widget=None):
         path = QPainterPath(QPointF(0, 0))
-        path.lineTo(self.mapFromScene(self.end_socket.get_position()))
+
+        if self.end_socket:
+            end = self.mapFromScene(self.end_socket.get_position())
+        else:
+            end = self.mapFromScene(*self.mouse_pos)
+
+        path.lineTo(end)
         self.setPath(path)
 
         painter.setPen(self._pen_selected if self.isSelected() else self._pen)
@@ -40,8 +60,11 @@ class NodeEdgeGraphics(QGraphicsPathItem):
     def boundingRect(self):
         return self.shape().boundingRect()
 
+    def __str__(self) -> str:
+        return class_id('NodeEdge', self)
+
 
 class NodeEdge:
-    def __init__(self, start_socket, end_socket):
+    def __init__(self, view, start_socket, end_socket):
 
-        self.edge_graphics = NodeEdgeGraphics(start_socket, end_socket)
+        self.edge_graphics = NodeEdgeGraphics(view, start_socket, end_socket)

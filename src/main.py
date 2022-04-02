66792c0a3425ea941e85d5e41a23380f2a33bdd7
virgilsisoe <virgilsisoe@gmail.com>
2022-04-02 12:53:29 -0400

    new sample node

--- src/main.py
+++ src/main.py
@@ -39,7 +39,10 @@ class NodeEditor(QWidget):
 
     def _debug_add_nodes(self):
         node1 = nodes.NodeExample1(self.scene)
-        node1.set_position(0, 0)
+        node1.set_position(-170, 0)
+
+        node3 = nodes.NodeExample1(self.scene)
+        node3.set_position(110, 0)
 
         node2 = nodes.NodeExample2(self.scene)
         node2.set_position(-450, -250)
@@ -47,7 +50,7 @@ class NodeEditor(QWidget):
         start_socket = node2.output_sockets[0]
         end_socket = node1.input_sockets[0]
 
-        NodeEdge(start_socket, end_socket)
+        NodeEdge(self.view, start_socket.socket_graphics, end_socket)
 
 
 class MainWindow(QMainWindow):
@@ -58,11 +61,13 @@ class MainWindow(QMainWindow):
 
         self.mouse_position = QLabel('')
         self.node_editor = NodeEditor()
-        self.node_editor.view.mouse_position.connect(
-            self.mouse_position.setText)
+        self.node_editor.view.mouse_position.connect(self.set_coords)
 
         self.setCentralWidget(self.node_editor)
         self.set_status_bar()
+
+    def set_coords(self, x, y):
+        self.mouse_position.setText(f'{round(x)}, {round(y)}')
 
     def set_status_bar(self):
         status_bar = self.statusBar()

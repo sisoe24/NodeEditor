ebb7b179277538485b3e7088f95f16f4fee4a561
virgilsisoe <virgilsisoe@gmail.com>
2022-04-03 19:25:13 -0400

    delete edge and some refactoring

--- src/widgets/node_edge.py
+++ src/widgets/node_edge.py
@@ -1,3 +1,4 @@
+import abc
 import logging
 
 from PySide2.QtCore import QPointF,  Qt
@@ -6,7 +7,6 @@ from PySide2.QtGui import QPen, QPainterPath, QColor, QPainterPathStroker
 from PySide2.QtWidgets import (
     QGraphicsPathItem,
     QGraphicsItem,
-    QGraphicsView
 )
 
 from src.utils import class_id
@@ -17,17 +17,22 @@ LOGGER.setLevel(logging.DEBUG)
 
 class NodeEdgeGraphics(QGraphicsPathItem):
 
-    def __init__(self, view, start_socket, end_socket):
-        super().__init__(start_socket)
+    def __init__(self, edge):
+        super().__init__(edge.start_point)
         LOGGER.debug('Creating edge')
 
-        self.end_socket = end_socket
+        self.edge = edge
 
         self._mouse_position = self.mapToScene(QPointF(0, 0))
-        view.mouse_position.connect(self._set_mouse)
+        self.edge._view.mouse_position.connect(self._set_mouse)
 
         self._set_colors()
         self._set_flags()
+
+    def delete_edge(self):
+        """Delete the graphic edge and remove its reference from siblings nodes."""
+        self.edge.clear_end_points()
+        self.scene().removeItem(self)
 
     def _set_colors(self):
         self._pen_selected = QPen(QColor('#DD8600'))
@@ -51,8 +56,8 @@ class NodeEdgeGraphics(QGraphicsPathItem):
 
         path = QPainterPath(QPointF(0, 0))
 
-        if self.end_socket:
-            end = self.mapFromScene(self.end_socket.get_position())
+        if self.edge.end_point:
+            end = self.mapFromScene(self.edge.end_point.get_position())
         else:
             end = self.mapFromScene(self._mouse_position)
 
@@ -71,11 +76,59 @@ class NodeEdgeGraphics(QGraphicsPathItem):
         return class_id('NodeEdge', self)
 
 
-class NodeEdge:
-    def __init__(self, view, start_socket, end_socket):
-        self.edge_graphics = NodeEdgeGraphics(view, start_socket, end_socket)
-        end_socket.parentItem().add_edge(self)
+class _EdgeInterface(abc.ABC):
 
-class NodeEdgeTmp:
+    @property
+    @abc.abstractmethod
+    def start_point(self):
+        pass
+
+    @property
+    def end_point(self):
+        return None
+
+
+class NodeEdge(_EdgeInterface):
+    def __init__(self, view, start_socket, end_socket):
+
+        self._start_socket = start_socket
+        self._end_socket = end_socket
+        self._view = view
+
+        self.edge_graphics = NodeEdgeGraphics(self)
+        self._add_end_points()
+
+    @property
+    def start_point(self):
+        return self._start_socket
+
+    @property
+    def end_point(self):
+        return self._end_socket
+
+    def clear_end_points(self):
+        # TODO: might need to clean a specific edge when multiple are present
+        self.start_point.parentItem()._edges.clear()
+        self.end_point.parentItem()._edges.clear()
+
+    def _add_end_points(self):
+        """Add the edge to the end points.
+
+        When creating the edge, automatically add its reference to the end points
+        (starting socket and end socket) `edges` attribute. This is to be able
+        to have a reference when deleting the nodes.
+        """
+        self._end_socket.parentItem().add_edge(self)
+        self._start_socket.parentItem().add_edge(self)
+
+
+class NodeEdgeTmp(_EdgeInterface):
     def __init__(self, view, start_socket):
-        self.edge_graphics = NodeEdgeGraphics(view, start_socket, None)
+
+        self._start_socket = start_socket
+        self._view = view
+        self.edge_graphics = NodeEdgeGraphics(self)
+
+    @property
+    def start_point(self):
+        return self._start_socket

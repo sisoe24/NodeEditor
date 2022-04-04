37767648125c19c8d9c073ef55c4487a72cd813b
virgilsisoe <virgilsisoe@gmail.com>
2022-04-04 19:25:10 -0400

    expose edge object

--- src/widgets/node_socket.py
+++ src/widgets/node_socket.py
@@ -6,6 +6,7 @@ from PySide2.QtWidgets import (
 )
 
 from src.utils import class_id
+from src.widgets.node_edge import NodeEdge
 
 
 class SocketGraphics(QGraphicsItem):
@@ -15,8 +16,26 @@ class SocketGraphics(QGraphicsItem):
         self._outline_pen = QPen(Qt.black)
         self._outline_pen.setWidthF(0.5)
 
+        self._edge = None
+
         self._draw_graphics()
         self._set_flags()
+
+    @property
+    def edge(self):
+        return self._edge
+
+    @edge.setter
+    def edge(self, edge: NodeEdge):
+        self._edge = edge
+
+    def has_edge(self):
+        return bool(self.edge)
+
+    def remove_edge(self):
+        self.edge.clear_reference()
+        self.scene().removeItem(self.edge.edge_graphics)
+        self.edge = None
 
     def _set_flags(self):
         """Initialize UI for the Node graphic content."""
@@ -38,7 +57,7 @@ class SocketGraphics(QGraphicsItem):
         return self._socket_body.boundingRect()
 
     def __str__(self) -> str:
-        return class_id('Socket', self)
+        return class_id('SocketGraphics', self)
 
     def get_position(self):
         """Get socket position in the scene."""

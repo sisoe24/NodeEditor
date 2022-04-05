c1a9780b7625fbd13d98104fe5b9a2d48ee56680
virgilsisoe <virgilsisoe@gmail.com>
2022-04-05 15:55:42 -0400

    save/load file initial implementation

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
+import os
 import sys
+import json
 import logging
+from pprint import pprint
 
+from PySide2.QtCore import (
+    QSettings,
+    QCoreApplication
+)
 from PySide2.QtWidgets import (
+    QGraphicsScene,
+    QAction,
+    QToolBar,
     QLabel,
     QApplication,
     QWidget,
@@ -12,8 +22,10 @@ from PySide2.QtWidgets import (
 
 from src.widgets.editor_scene import Scene
 from src.widgets.editor_view import GraphicsView
-from src.examples.nodes import *
 from src.widgets.node_edge import NodeEdge
+from src.widgets.node_graphics import NodeGraphics
+
+from src.examples.nodes import *
 
 LOGGER = logging.getLogger('nodeeditor.main')
 
@@ -38,6 +50,7 @@ class NodeEditor(QWidget):
         self._debug_add_nodes()
 
     def _debug_add_nodes(self):
+        return
 
         node_test = nodes.NodeExample3(self.scene)
         node_test.set_position(-245, 0)
@@ -47,7 +60,6 @@ class NodeEditor(QWidget):
 
         node_example = nodes.NodeExample1(self.scene)
         node_example.set_position(-75, 0)
-        return
 
         # create debug edge
         start_socket = node_example.output_sockets[0]
@@ -75,12 +87,95 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("NodeEditor")
         self.setGeometry(-1077.296875, -5.31640625, 1080, 1980)
 
+        # QCoreApplication.setOrganizationName('virgilsisoe')
+        # QCoreApplication.setOrganizationDomain('nodeeditor.com')
+        # QCoreApplication.setApplicationName('Node Editor')
+
+        self.settings = QSettings(os.path.join(
+            os.getcwd(), 'NodeEditor.ini'), QSettings.IniFormat)
+
         self.mouse_position = QLabel('')
         self.node_editor = NodeEditor()
         self.node_editor.view.mouse_position.connect(self.set_coords)
 
+        # i = self.saveState(1)
+        # self.restoreState(i)
+        # print("➡ i :", i)
+        self._add_actions()
+
         self.setCentralWidget(self.node_editor)
         self.set_status_bar()
+
+        # self.check_state()
+        self.load_file()
+
+    def check_state(self):
+        scene: QGraphicsScene = self.node_editor.scene.graphics_scene
+
+        nodes = [x for x in scene.items() if isinstance(x, NodeGraphics)]
+
+        self.settings.beginGroup('state')
+        state = {}
+        for item in nodes:
+            print("➡ item :", item)
+
+            # self.settings.setValue('abc', item.node)
+            # state[str(item.node)] = {item}
+        self.settings.endGroup()
+        pprint(state)
+
+        # with open('save_file.json', 'w') as file:
+        #     json.dump(state, file)
+
+    def load_file(self):
+
+        with open('save_file.json', 'r') as file:
+            data = json.load(file)
+
+        node_edges = {}
+
+        for node_objects in data['nodes']:
+            for node_type, node_attributes in node_objects.items():
+
+                # Review: is there a better way?
+                create_node = getattr(nodes, node_attributes['class'])
+
+                node = create_node(self.node_editor.scene)
+                node.set_position(node_attributes['position']['x'],
+                                  node_attributes['position']['y'])
+
+                edges = node_attributes.get('edges', {})
+                node_edges[node_type] = {'obj': node, 'edges': edges}
+
+        # pprint(node_edges)
+
+        for node, connections in node_edges.items():
+            obj = connections['obj']
+            for edge in connections['edges'].values():
+                start_socket = obj.output_sockets[edge['start_socket']]
+
+                end_edge = edge['end_socket']
+                end_node = end_edge.get('node')
+                end_socket_obj = node_edges.get(end_node).get('obj')
+                end_socket = end_socket_obj.input_sockets[end_edge['socket']]
+
+                NodeEdge(self.node_editor.view,
+                         start_socket.socket_graphics,
+                         end_socket.socket_graphics)
+            break
+
+    def _add_actions(self):
+        save = QAction('Save File', self)
+        save.triggered.connect(self.check_state)
+
+        load = QAction('Load File', self)
+        load.triggered.connect(self.load_file)
+
+        toolbar = QToolBar()
+        toolbar.setStyleSheet('color: white;')
+        toolbar.addAction(save)
+        toolbar.addAction(load)
+        self.addToolBar(toolbar)
 
     def set_coords(self, x, y):
         self.mouse_position.setText(f'{round(x)}, {round(y)}')

c9b84ae1a9e6ff1fc37c237b013f591c7bcd0584
virgilsisoe <virgilsisoe@gmail.com>
2022-04-10 15:20:34 -0400

    added some debug widgets

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 import sys
+import json
+import random
 import logging
 
+from PySide2.QtGui import (
+    QKeySequence
+)
 
 from PySide2.QtWidgets import (
+    QPushButton,
+    QUndoView,
+    QUndoStack,
     QAction,
     QToolBar,
     QLabel,
@@ -16,8 +24,9 @@ from PySide2.QtWidgets import (
 from src.widgets.editor_scene import Scene
 from src.widgets.editor_view import GraphicsView
 from src.widgets.node_edge import NodeEdge
-
-from src.utils.graph_state import load_file, save_file
+from src.widgets.logic.undo_redo import (
+    AddNodeCommand, LoadCommand, SaveCommand
+)
 
 from src.examples.nodes import *
 
@@ -25,14 +34,16 @@ LOGGER = logging.getLogger('nodeeditor.main')
 
 
 class NodeEditor(QWidget):
-    def __init__(self):
-        QWidget.__init__(self)
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.undo_stack = QUndoStack()
 
         # Create a graphic scene
         self.scene = Scene()
 
         # Create a graphic view
-        self.view = GraphicsView(self.scene.graphics_scene)
+        self.view = GraphicsView(self.scene.graphics_scene, self)
 
         _layout = QVBoxLayout()
         _layout.setSpacing(5)
@@ -45,9 +56,15 @@ class NodeEditor(QWidget):
 
     def _debug_add_nodes(self):
 
-        return
-        node_test = nodes.NodeTest(self.scene)
+        node_test = nodes.NodeDebug(self.scene.graphics_scene)
         node_test.set_position(-245, 0)
+
+        # node_test = nodes.NodeDebug(self.scene.graphics_scene)
+        # node_test.set_position(-145, 0)
+
+        node_test = nodes.NodeDebug(self.scene.graphics_scene)
+        node_test.set_position(-45, 0)
+        return
 
         node_debug = nodes.NodeDebug(self.scene)
         node_debug.set_position(95, 0)
@@ -69,17 +86,50 @@ class NodeEditor(QWidget):
         NodeEdge(start_socket_b.socket_graphics, end_socket_c.socket_graphics)
 
 
+class DebugWidget(QWidget):
+    def __init__(self, node_editor, undo_stack):
+        super().__init__()
+
+        self.node_editor = node_editor
+        self.scene = self.node_editor.scene.graphics_scene
+        self.undo_stack = undo_stack
+
+        btn = QPushButton('Add Node Example')
+        btn.clicked.connect(lambda: self.add_node(nodes.NodeExample))
+
+        btn1 = QPushButton('Add Node Debug')
+        btn1.clicked.connect(lambda: self.add_node(nodes.NodeDebug))
+
+        _layout = QVBoxLayout()
+
+        _layout.addWidget(self.node_editor)
+        _layout.addWidget(btn)
+        _layout.addWidget(btn1)
+        _layout.addWidget(QUndoView(self.undo_stack))
+
+        self.setLayout(_layout)
+
+    def add_node(self, node):
+        n1 = random.randint(-300, 300)
+        n2 = random.randint(-300, 300)
+        command = AddNodeCommand(self.scene, (0, 0), node, 'Add Node')
+        self.undo_stack.push(command)
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
-        QMainWindow.__init__(self)
+        super().__init__()
         self.setWindowTitle("NodeEditor")
         self.setGeometry(-1077.296875, -5.31640625, 1080, 1980)
 
+        self.undo_stack = QUndoStack(self)
+
         self.mouse_position = QLabel('')
-        self.node_editor = NodeEditor()
+        self.node_editor = NodeEditor(self)
         self.node_editor.view.mouse_position.connect(self.set_coords)
 
-        self.setCentralWidget(self.node_editor)
+        widget = DebugWidget(self.node_editor, self.undo_stack)
+        self.setCentralWidget(widget)
 
         self._scene = self.node_editor.scene.graphics_scene
         self._add_actions()
@@ -88,21 +138,37 @@ class MainWindow(QMainWindow):
         # load_file(self._scene)
         # save_file(self._scene)
 
+    def load_file(self):
+        command = LoadCommand(self._scene, 'Load File')
+        self.undo_stack.push(command)
+
+    def save_file(self):
+        command = SaveCommand(self._scene, 'Save File')
+        self.undo_stack.push(command)
+
     def _add_actions(self):
         new = QAction('New File', self)
         new.triggered.connect(self._scene.clear)
 
         save = QAction('Save File', self)
-        save.triggered.connect(lambda: save_file(self._scene))
+        save.triggered.connect(self.save_file)
 
         load = QAction('Load File', self)
-        load.triggered.connect(lambda: load_file(self._scene))
+        load.triggered.connect(self.load_file)
+
+        undo = self.undo_stack.createUndoAction(self, 'Undo')
+        undo.setShortcut(QKeySequence.Undo)
+        redo = self.undo_stack.createRedoAction(self, 'Redo')
+        redo.setShortcut(QKeySequence.Redo)
 
         toolbar = QToolBar()
         toolbar.setStyleSheet('color: white;')
+        toolbar.addAction(undo)
+        toolbar.addAction(redo)
         toolbar.addAction(new)
         toolbar.addAction(save)
         toolbar.addAction(load)
+
         self.addToolBar(toolbar)
 
     def set_coords(self, x, y):

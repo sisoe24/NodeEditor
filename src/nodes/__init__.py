c2422f90717821246ad46d2b8f4951d9e3c13bf6
virgilsisoe <virgilsisoe@gmail.com>
2022-04-26 11:30:47 -0400

    .

--- src/nodes/__init__.py
+++ src/nodes/__init__.py
@@ -2,6 +2,7 @@ from .nodes_register import NodesRegister
 from .nodes_content import NodeContent
 from .nodes_utility import (
     connect_output_edges,
+    connect_input_edges,
     extract_output_edges,
     extract_input_edges,
     create_node
